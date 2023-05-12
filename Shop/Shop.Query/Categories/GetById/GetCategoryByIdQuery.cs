819160808ba7e2d59d4ddf54d2b993c02f020548
Masoumeh kazemi <masoumehkazemi2023@gmail.com>
2023-05-12 14:19:09 +0430

    category query

---
+++ Shop/Shop.Query/Categories/GetById/GetCategoryByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Common.Applications;
+using Common.Query;
+using Shop.Domain.CategoryAgg;
+using Shop.Query.Categories.DTOs;
+
+namespace Shop.Query.Categories.GetById;
+
+public record GetCategoryByIdQuery(long CategoryId) : IQuery<CategoryDto>;
---
+++ Shop/Shop.Query/Categories/GetList/GetCategoryListQuery.cs
@@ -0,0 +1,24 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Categories.DTOs;
+
+namespace Shop.Query.Categories.GetList;
+
+public record GetCategoryListQuery : IQuery<List<CategoryDto>>;
+
+internal class GetCategoryListQueryHandler : IQueryHandler<GetCategoryListQuery, List<CategoryDto>>
+{
+    private readonly ShopContext _context;
+
+    public GetCategoryListQueryHandler(ShopContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<CategoryDto>> Handle(GetCategoryListQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _context.Categories.OrderByDescending(d => d.Id).ToListAsync(cancellationToken);
+        return result.Map();
+    }
+}
