cd574ebc7eceb6186c2c1733aa43bf3930d0ab90
Masoumeh kazemi <masoumehkazemi2023@gmail.com>
2023-05-13 14:51:07 +0430

    get comment by filter ang by id

---
+++ Shop/Shop.Query/Comments/GetById/GetCommentByIdQuery.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Comments.DTOs;
+
+namespace Shop.Query.Comments.GetById;
+
+public record GetCommentByIdQuery(long CommentId) : IQuery<CommentDto?>;
+internal class GetCommentByIdQueryHandler : IQueryHandler<GetCommentByIdQuery, CommentDto?>
+{
+    private readonly ShopContext _context;
+
+    public GetCommentByIdQueryHandler(ShopContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CommentDto?> Handle(GetCommentByIdQuery request, CancellationToken cancellationToken)
+    {
+        var comment = await _context.Comments.FirstOrDefaultAsync(f => f.Id == request.CommentId, cancellationToken: cancellationToken);
+
+        return comment.Map();
+    }
+}
